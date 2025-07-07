a914b42c152c8b92c709fc34a89cf715ad632d1f
AhmedRazaSidd <ar5812264@gmail.com>
2025-07-07 15:14:16 -0700

    Project Initialize and half chat bot ui added

---
+++ app/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import LandingSections from "@/components/LandingSections";
+import { useEffect, useRef, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+
+import {
+  X,
+  MessageCircle,
+  Send,
+  Loader2,
+  ArrowDownCircleIcon,
+} from "lucide-react";
+
+import { motion, AnimatePresence } from "framer-motion";
+import { useChat } from "@ai-sdk/react";
+import { set } from "date-fns";
+
+export default function Chat() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [showChatIcon, setShowChatIcon] = useState(false);
+  const chatIconRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setShowChatIcon(true);
+      } else {
+        setShowChatIcon(false);
+        setIsChatOpen(false);
+      }
+    };
+
+    // handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleChat = () => {
+    setIsChatOpen(!isChatOpen);
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <LandingSections />
+      <AnimatePresence>
+        {showChatIcon && (
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 100 }}
+            transition={{ duration: 0.2 }}
+            className="fixed bottom-4 right-4 z-50"
+          >
+            <Button
+              ref={chatIconRef}
+              onClick={toggleChat}
+              className="rounded-full size-14 p-2 shadow-lg"
+            >
+              {!isChatOpen ? (
+                <MessageCircle className="size-12" />
+              ) : (
+                <ArrowDownCircleIcon />
+              )}
+            </Button>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {isChatOpen && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 1, scale: 0.8 }}
+            transition={{ duration: 0.2 }}
+            className="fixed bottom-20 right-4 z-50 w-[95%] md:w-[500px]"
+          >
+            <Card className="border-2">
+              <CardHeader className="flex items-center justify-between">
+                <CardTitle className="text-lg font-medium">
+                  Chat with AI
+                </CardTitle>
+                <Button
+                  onClick={toggleChat}
+                  size="icon"
+                  className="rounded-full size-14 p-2 shadow-lg"
+                >
+                  <X className="size-12" />
+                </Button>
+              </CardHeader>
+            </Card>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
